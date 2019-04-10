90083b81e6805eec917c0a02962b1bf655588202
Osama Mehmood <osama@Osamas-iMac.local>
2019-04-11 09:01:53 +1000

    Simple scaffold that includes a text field and button to add a task.

--- src/Main.elm
+++ src/Main.elm
@@ -3,93 +3,29 @@ import Html exposing (..)
 import Html.Events exposing (onClick, onInput)
 import Html.Attributes exposing (style, type_)
 
-main : Program () (Model) Msg
 main = 
-    Browser.sandbox {init = { tasks = [], taskBox = "" }, update = update, view = view}
+    Browser.sandbox {init = [], update = update, view = view}
 
 
-type Msg = AddTask | UpdateTextBox String
-
-type alias Model =
-    { tasks : List String
-    , taskBox : String 
-    }
-
+type Msg = AddTask
 
 update msg model = 
     case msg of 
-    AddTask -> model 
+        AddTask -> model
 
-    UpdateTextbox string -> { model | taskBox = string }
 
-view model = 
-    div[]
-        [
-            h1
-                []
-                [
-                    text "To Do list" 
-                ]
-                , input 
-                    [
-                        type_ "text",
-                        onInput UpdateTextBox
-                    ]
-                    []
-                , button 
-                    []
-                    [
-                        text "Add"
-                    ]
+view model =
+    div []
+        [ h1 
+            []
+            [ text "My Todo List" ]
+        , input 
+            [ type_ "text" 
+            ]
+            []
+        , button
+            [ onClick AddTask
+            ]
+            [ text "add" ]
         ]
 
-
-
-
-
-
--- import Browser
--- import Html exposing (..)
--- import Html.Events exposing (onClick, onInput)
--- import Html.Attributes exposing (style, type_)
-
-
--- main : Program () (Model) Msg
--- main = 
---     Browser.sandbox {init = { tasks = [], taskBox = "" }, update = update, view = view}
-
--- type Msg 
---     = AddTask 
---     | UpdateTextbox String
-
-
--- type alias Model =
---     { tasks : List String
---     , taskBox : String 
---     }
-
-
--- update : Msg -> Model -> Model
--- update msg model = 
---     case msg of 
---         AddTask -> model
-
---         UpdateTextbox string -> { model | taskBox = string }
-
-
--- view : Model -> Html Msg
--- view model =
---     div []
---         [ h1 
---             []
---             [ text "My Todo List" ]
---         , input 
---             [ type_ "text" 
---             , onInput UpdateTextbox
---             ]
---             []
---         , button
---             [ onClick AddTask
---             ]
---             [ text "add" ]
---         ]
