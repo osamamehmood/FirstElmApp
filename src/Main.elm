be5fd07719d161f856f50b54d843b3abcd516d30
Osama Mehmood <osama@Osamas-iMac.local>
2019-04-11 11:17:21 +1000

    fire an event onClick to add the task task list

--- src/Main.elm
+++ src/Main.elm
@@ -1,32 +1,41 @@
 import Browser
 import Html exposing (..)
 import Html.Events exposing (onClick, onInput)
-import Html.Attributes exposing (style, type_)
+import Html.Attributes exposing (style, type_, value)
 
+main : Program() (Model) Msg
 main = 
-    Browser.sandbox {init = { task = [], taskBox = ""}, update = update, view = view}
+    Browser.sandbox {init = { tasksList = [], taskBox = ""}, update = update, view = view}
 
 
-type Msg = AddTask | UpdateTaskBox String
+type Msg = AddTask String | UpdateTaskBox String
 
+type alias Model = 
+    { tasksList : List String
+    , taskBox : String
+    }
+
+update : Msg -> Model -> Model
 update msg model = 
     case msg of 
-        AddTask -> model
-        UpdateTaskBox string -> { model | taskBox = string }
+        AddTask string ->  { model | tasksList = string :: model.tasksList, taskBox ="" }
+        UpdateTaskBox string -> { model | taskBox = string } 
 
 
+view : Model -> Html Msg
 view model =
     div []
         [ h1 
             []
             [ text "My Todo List" ]
         , input 
-            [ type_ "text",
-              onInput UpdateTaskBox
+            [ type_ "text"
+            , value model.taskBox
+            , onInput UpdateTaskBox
             ]
             []
         , button
-            [ onClick AddTask
+            [ onClick (AddTask model.taskBox)
             ]
             [ text "add" ]
         ]
