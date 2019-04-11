40db924c677e65d7a155a49762daa05125e81d5e
Osama Mehmood <osama@Osamas-iMac.local>
2019-04-11 14:15:53 +1000

    Display tasks individually

--- src/Main.elm
+++ src/Main.elm
@@ -3,9 +3,11 @@ import Html exposing (..)
 import Html.Events exposing (onClick, onInput)
 import Html.Attributes exposing (style, type_, value)
 
+
+
 main : Program() (Model) Msg
 main = 
-    Browser.sandbox {init = { tasksList = [], taskBox = ""}, update = update, view = view}
+    Browser.sandbox { init = { tasksList = [], taskBox = ""}, update = update, view = view }
 
 
 type Msg = AddTask String | UpdateTaskBox String
@@ -18,8 +20,19 @@ type alias Model =
 update : Msg -> Model -> Model
 update msg model = 
     case msg of 
-        AddTask string ->  { model | tasksList = string :: model.tasksList, taskBox ="" }
+        AddTask string->  { model | tasksList = string :: model.tasksList, taskBox = ""}
         UpdateTaskBox string -> { model | taskBox = string } 
+
+
+renderTasksItem : String -> Html Msg   
+renderTasksItem item = 
+    li 
+        []
+        [ input 
+            [ type_ "checkbox"]
+            []
+        , text item
+        ]
 
 
 view : Model -> Html Msg
@@ -30,13 +43,15 @@ view model =
             [ text "My Todo List" ]
         , input 
             [ type_ "text"
-            , value model.taskBox
             , onInput UpdateTaskBox
+            , value model.taskBox
             ]
             []
         , button
-            [ onClick (AddTask model.taskBox)
-            ]
+            [ onClick (AddTask model.taskBox)]
             [ text "add" ]
+        , ul [] 
+            (
+                List.map renderTasksItem model.tasksList
+            )
         ]
-
